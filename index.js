59c8a68746a96a04dbd2ffd46ec781c2790f6e56
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-07 16:37:32 +0300

    commit tree f1325af129a457d9da5b12941e908b611d6d34b1

---
+++ index.js
@@ -0,0 +1,56 @@
+	function shout(string) {
+	return string.toUpperCase();
+	}
+	
+	function whisper(string) {
+	return string.toLowerCase();
+	}
+	
+	function logShout(string){
+	console.log('HELLO'.toUpperCase());
+	}
+	
+	function logWhisper(string){
+	console.log('hello'.toLowerCase());
+	}
+	
+
+	function sayHiToHeadphonedRoommate(string){
+		if (string.toLowerCase() === string)
+			return "I can't hear you!";
+			else if(string.toUpperCase() === string)
+			return "YES INDEED!";
+			else(string === "Let's have dinner together!")
+			return "I would love to!";	
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
